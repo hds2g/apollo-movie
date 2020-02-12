c6a57b9592a2f15f7842d0b795b90d6a9277c000
DS Hwang <hds2g22@gmail.com>
2020-02-13 07:47:15 +0900

    Detail: GET_MOVIE Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const Container = styled.div`
   display: flex;
@@ -55,6 +56,9 @@ export default () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
     </Container>
   );
 };
