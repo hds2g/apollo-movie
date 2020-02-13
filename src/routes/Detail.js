f1bde78b2e9764338b2466a071806a822b566e52
DS Hwang <hds2g22@gmail.com>
2020-02-13 14:48:32 +0900

    Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -7,7 +7,6 @@ import styled from "styled-components";
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
       language
@@ -29,6 +28,7 @@ const Container = styled.div`
 
 const Column = styled.div`
   margin-left: 10px;
+  width: 50%;
 `;
 
 const Title = styled.h1`
@@ -49,22 +49,34 @@ const Poster = styled.div`
   width: 25%;
   height: 60%;
   background-color: transparent;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
 `;
 
 export default () => {
   let { id } = useParams();
-  console.log(typeof id);
   id = parseInt(id);
-  const { loading, data } = useQuery(GET_MOVIE, { variables: { id } });
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }
+  });
 
   return (
     <Container>
       <Column>
-        <Title>Name</Title>
-        <Subtitle>English · 4.5</Subtitle>
-        <Description>lorem ipsum lalalla </Description>
+        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        {!loading && data.movie && (
+          <>
+            <Subtitle>
+              {data.movie.language} · {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )}
       </Column>
-      <Poster></Poster>
+      <Poster
+        bg={data && data.movie ? data.movie.medium_cover_image : ""}
+      ></Poster>
     </Container>
   );
 };
